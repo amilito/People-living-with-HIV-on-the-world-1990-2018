567fb368f025c6988cc7943c94e0719d1b6c9ff8
amilito <55156267+amilito@users.noreply.github.com>
2023-01-29 20:26:34 +0100

    Update map.R

--- map.R
+++ map.R
@@ -6,19 +6,23 @@ library(rworldmap)
 library(dplyr)
 library(animation)
 library("readxl")
+library(RColorBrewer)
+library(classInt)
 
 my_data <- read_excel(file.choose())
 nc <- dim(my_data)[2]
 
 new_names <-
-  my_data %>% names(.) %>% .[5:nc] %>% lapply(., substr, start = 1, stop = 4) %>% unlist(.)
+  my_data %>% 
+  names(.) %>% 
+  .[5:nc] %>% 
+  lapply(., substr, start = 1, stop = 4) %>% 
+  unlist(.)
 
 l <- length(new_names)
 names(my_data)[5:nc] <- new_names
 
-
-missing_values <- function(what, substitution, data)
-{
+missing_values <- function(what, substitution, data){
   data[data == what] <- substitution
   return(data)
 }
@@ -26,24 +30,25 @@ missing_values <- function(what, substitution, data)
 part <- my_data[, 1:4]
 
 my_data_preprocess <-
-  my_data %>% missing_values("..", NA, .) %>% .[, 5:33] %>% lapply(., function(x)
-    as.numeric(as.character(x))) %>% cbind(part, .)
+  my_data %>% 
+  missing_values("..", NA, .) %>%
+  .[, 5:33] %>% 
+  lapply(., function(x) as.numeric(as.character(x))) %>% 
+  cbind(part, .)
 
 par(mai = c(0, 0, 0.2, 0),
     xaxs = "i",
     yaxs = "i")
 
-library(RColorBrewer)
-library(classInt)
-
 colourPalette <- RColorBrewer::brewer.pal(5, "YlGn")
 
-
-
 dataformap <-
-  my_data %>% missing_values("..", NA, .) %>% .[, 5:33] %>% lapply(., function(x)
-    as.numeric(as.character(x))) %>% cbind(part, .) %>% joinCountryData2Map(., joinCode =
-                                                                              "NAME", nameJoinColumn = "Country Name")
+  my_data %>% 
+  missing_values("..", NA, .) %>% 
+  .[, 5:33] %>% 
+  lapply(., function(x) as.numeric(as.character(x))) %>% 
+  cbind(part, .) %>% 
+  joinCountryData2Map(., joinCode = "NAME", nameJoinColumn = "Country Name")
 
 #getting class intervals using a "jenks" classification in classInt package
 #classInt <- classInt::classIntervals( dataformap[["1995"]], n=5, style="jenks")
@@ -75,8 +80,7 @@ for (i in 1:l) {
       labelFontSize = 1
     )
   )
-  ani.record() # record the current frame
-  
+  ani.record() # record the current frame  
 }
 
 oopts = ani.options(interval = 0.5)
